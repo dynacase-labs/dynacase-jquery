bec02887e35bdc029418a993144dcd0384412cd6
Charles Bonnissent <charles.bonnissent@anakeen.com>
2015-12-11 10:51:36 +0100

    feat(2): add jquery 2 (refs #5883)

--- update.sh
+++ update.sh
@@ -6,12 +6,9 @@ bower install jquery=jquery#$JQUERY_VERSION
 PACKAGE_VERSION=$(bower info jquery#$JQUERY_VERSION version | tail -n 1 | cut -d "'" -f 2)
 cd ./bower_components
 mkdir ./jquery/$PACKAGE_VERSION
-mv ./jquery/jquery.js ./jquery/$PACKAGE_VERSION/
-mv ./jquery/jquery.min.js ./jquery/$PACKAGE_VERSION/
-mv ./jquery/README.md ./jquery/$PACKAGE_VERSION/
-mv ./jquery/MIT-LICENSE.txt ./jquery/$PACKAGE_VERSION/
-mv ./jquery/version.txt ./jquery/$PACKAGE_VERSION/
-zip -r "../jquery-$PACKAGE_VERSION.zip" ./jquery/$JQUERY_VERSION/
+mv ./jquery/dist/jquery.js ./jquery/$PACKAGE_VERSION/
+mv ./jquery/dist/jquery.min.js ./jquery/$PACKAGE_VERSION/
+zip -r "../jquery-$PACKAGE_VERSION.zip" ./jquery/$PACKAGE_VERSION/
 cd ..
 scp "jquery-$PACKAGE_VERSION.zip" distrib@ftp.anakeen.com:/share/ftp/third-party/
 echo $PACKAGE_VERSION > VERSION
